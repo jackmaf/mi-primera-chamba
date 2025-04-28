f91fc8137b4d073968e2f78766f18e2f2f277032
Jack Florez Jimenez <jackmaf0@gmail.com>
2025-04-28 15:16:37 -0500

    primera subida

---
+++ config/initializers/logger_patch.rb
@@ -0,0 +1,14 @@
+# Crear el módulo Severity para la clase Logger si no existe
+unless defined?(::Logger::Severity)
+  class ::Logger
+    module Severity
+      DEBUG = 0
+      INFO = 1
+      WARN = 2
+      ERROR = 3
+      FATAL = 4
+      UNKNOWN = 5
+    end
+    include Severity
+  end
+end 
